54b7f83fd3b757fd8eae9f47048d943a17cb8955
MOHD SABIR <sabiransari01981@gmail.com>
2024-01-05 15:33:46 +0530

    -

--- Hashmaps/characterCount.js
+++ Hashmaps/characterCount.js
@@ -1,8 +1,19 @@
+ // explaination -- 
+ // charCount[char] = (charCount[char] || 0) + 1;
+ //  here   charCount[char] is key   with the value  undefined 
+ //  so on each iteration  we are setting the value of each key 
+ //  { h : undefind, after 1 itration  it will become 1 
+ //    e : undefined  after 2 iteration it will become 1
+ //
+ //    l : undefined  after 3 iteration it will become 1
+ //    l : 1 , bcz it value was 1 so it will become 2
+ //  }
+
 function countCharacters(str) {
   const charCount = {};
   for (const char of str) {
     charCount[char] = (charCount[char] || 0) + 1;
-  }
+   }
   return charCount;
 }
 

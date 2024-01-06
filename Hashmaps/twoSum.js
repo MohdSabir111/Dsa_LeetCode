a3666be5aa165828c1d642c366ad81595ab971ad
MOHD SABIR <95029813+MohdSabir111@users.noreply.github.com>
2024-01-06 22:45:59 +0530

    Update twoSum.js
    adding explanations

--- Hashmaps/twoSum.js
+++ Hashmaps/twoSum.js
@@ -1,13 +1,18 @@
+// Explanation--  complement = 9-2 =7 , hasOwnProperty -its false so   { '2' : 0 }
+  // comlement = 9-11 = -2 ,  hasOwnProperty -its false  so  { '2' : 0,  '11' : 1 }
+//  comlement = 9-7= 2  ,  hasOwnProperty -its true so [0,2] answer
 function twoSum(nums, target) {
   const numIndices = {};
 
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
-
+    
+     // hasOwnProperty- check if there a "value" to this corresponding "key"   
     // Check if the complement exists in the numIndices object
+    
     if (numIndices.hasOwnProperty(complement)) {
-      // If yes, return the indices of the two numbers
       
+      // If yes, return the indices of the two numbers
       return [numIndices[complement], i];
     }
 
@@ -21,7 +26,7 @@ function twoSum(nums, target) {
 }
 
 // Example usage:
-const nums = [2, 7, 11, 15];
+const nums = [2, 11, 7, 15];
 const target = 9;
 const result = twoSum(nums, target);
 

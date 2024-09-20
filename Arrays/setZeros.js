fc8eb7b386f26a4422bf7ff9f3f0f8960f238094
MOHD SABIR <sabiransari01981@gmail.com>
2024-09-20 17:06:10 +0530

    find Maximum occurrences of an element in an array

---
+++ Arrays/setZeros.js
@@ -0,0 +1,34 @@
+// arra = [1,2,0,3,4,0,6,7]
+// out = [1,2,0,0,3,4,0,0]
+
+// let array = [1,2,0,3,4,0,6,7]
+
+// function inserEle(array,pos,ele){
+  
+//   for(let i = array.length-1; i>=pos; i--){
+//     array[i] = array[i-1]
+   
+//   }
+//   array[pos] = ele;
+//   return array
+// }
+
+
+// //let array = [1,2,0,0,0,3,4,0]
+// function setzero(array){
+//   for( let i= 0; i<array.length-1; i++){
+//     if(array[i]==0){
+//       inserEle(array,i,0)
+//       i++
+//     }
+//   }
+//   return array
+// }
+
+// let res = setzero(array)
+// console.log(res)
+
+
+
+//out = [1,2,-1,3,4,-1,6,7,-1,-1]
+
